cad2605bf0be337f87b721b937c5520e770fc39a
Sushma S <sushma.s@cspl.com>
2023-06-19 14:13:15 +0530

    sushma_1.0.0

---
+++ Models/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_application.Models
+{
+    public class Job
+    {
+        public string company_name;
+        public int yoe;
+        public string job_type;
+        public string location;
+        public string shift;
+        public string skillset;
+        public Job(string company_name, int yoe, string job_type, string location, string shift, string skillset)
+        {
+            this.company_name = company_name;
+            this.yoe = yoe;
+            this.job_type = job_type;
+            this.location = location;
+            this.shift = shift;
+            this.skillset = skillset;
+        }
+
+    }
+}
